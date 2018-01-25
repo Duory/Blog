24e10f83b0ff720e0e046590991e6fa32fd3adde
Duory <makovoi94@gmail.com>
2018-01-25 21:11:55 +0700

    comments dao imp

---
+++ app/src/main/java/home/mva/blog/data/source/comment/CommentsDao.java
@@ -0,0 +1,28 @@
+package home.mva.blog.data.source.comment;
+
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+import home.mva.blog.data.model.Comment;
+
+public interface CommentsDao {
+
+    @Query("SELECT * FROM comments")
+    List<Comment> getComments();
+
+    @Query("SELECT * FROM comments WHERE postid=:postId")
+    List<Comment> getCommentsForPost(final int postId);
+
+    @Query("SELECT * FROM comments WHERE id=:commentId")
+    Comment getCommentById(String commentId);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertComment(Comment comment);
+
+    @Delete
+    void deleteComment(Comment comment);
+}
