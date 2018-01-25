3cb0db59a87f57f5e13b678c2e355dc8de522ede
Duory <makovoi94@gmail.com>
2018-01-25 18:41:18 +0700

    Comment model add

---
+++ app/src/main/java/home/mva/blog/data/model/Comment.java
@@ -0,0 +1,68 @@
+package home.mva.blog.data.model;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+/**
+ * Model class for a Comment.
+ */
+@Entity(tableName = "comments")
+public class Comment {
+
+    @PrimaryKey
+    @ColumnInfo(name = "id")
+    private Integer mId;
+
+    @ColumnInfo(name = "postid")
+    private Integer mPostId;
+
+    @ColumnInfo(name = "name")
+    private String mName;
+
+    @ColumnInfo(name = "email")
+    private String mEmail;
+
+    @ColumnInfo(name = "body")
+    private String mBody;
+
+    public Integer getId() {
+        return mId;
+    }
+
+    public void setId(Integer mId) {
+        this.mId = mId;
+    }
+
+    public Integer getPostId() {
+        return mPostId;
+    }
+
+    public void setPostId(Integer mPostId) {
+        this.mPostId = mPostId;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String mName) {
+        this.mName = mName;
+    }
+
+    public String getEmail() {
+        return mEmail;
+    }
+
+    public void setEmail(String mEmail) {
+        this.mEmail = mEmail;
+    }
+
+    public String getBody() {
+        return mBody;
+    }
+
+    public void setBody(String mBody) {
+        this.mBody = mBody;
+    }
+}
