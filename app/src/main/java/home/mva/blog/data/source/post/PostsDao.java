5e58eb144bb79b82a4b512044b20dbce1288ae87
Duory <makovoi94@gmail.com>
2018-01-25 20:48:33 +0700

    posts DAO renamed

--- app/src/main/java/home/mva/blog/data/source/post/PostsDao.java
+++ app/src/main/java/home/mva/blog/data/source/post/PostsDao.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 import home.mva.blog.data.model.Post;
 
-public interface PostDao {
+public interface PostsDao {
 
     @Query("SELECT * FROM posts")
     List<Post> getPosts();
