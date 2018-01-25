5d72ae1baac73b259fc77b50b1d9c0e9b6b72dc5
Duory <makovoi94@gmail.com>
2018-01-25 19:13:15 +0700

    posts data source interface add

---
+++ app/src/main/java/home/mva/blog/data/source/post/PostsDataSource.java
@@ -0,0 +1,34 @@
+package home.mva.blog.data.source.post;
+
+import java.util.List;
+
+import home.mva.blog.data.model.Post;
+
+public interface PostsDataSource {
+
+    interface GetPostsCallback {
+
+        void onPostsLoaded(List<Post> posts);
+
+        void onDataNotAvailable();
+    }
+
+    interface GetPostCallback {
+
+        void onPostLoaded(Post post);
+
+        void onDataNotAvailable();
+    }
+
+    void getPosts(GetPostsCallback callback);
+
+    void getPost(GetPostCallback callback);
+
+    void addPost(Post post);
+
+    void updatePost(Post post);
+
+    void deletePost(Post post);
+
+    void refreshPosts();
+}
