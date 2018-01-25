b4662fde62996200a8f2bfa7b1a7759856c35f39
Duory <makovoi94@gmail.com>
2018-01-25 19:45:38 +0700

    comments data source interface add

---
+++ app/src/main/java/home/mva/blog/data/source/comment/CommentsDataSource.java
@@ -0,0 +1,36 @@
+package home.mva.blog.data.source.comment;
+
+import java.util.List;
+
+import home.mva.blog.data.model.Comment;
+
+public interface CommentsDataSource {
+
+    interface GetCommentsCallback {
+
+        void onCommentsLoaded(List<Comment> comments);
+
+        void onDataNotAvailable();
+    }
+
+    interface GetCommentCallback {
+
+        void onCommentLoaded(Comment comment);
+
+        void onDataNotAvailable();
+    }
+
+    void getComments(GetCommentsCallback callback);
+
+    void getCommentsByPostId(String postId, GetCommentsCallback callback);
+
+    void getComment(String commentId, GetCommentCallback callback);
+
+    void addComment(Comment comment);
+
+    void updateComment(Comment comment);
+
+    void deleteComment(Comment comment);
+
+    void refreshComments();
+}
