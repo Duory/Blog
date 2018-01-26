ef4318c310c86f9c971788e579c76860d2e9eddf
Duory <makovoi94@gmail.com>
2018-01-26 14:48:59 +0700

    main thread executor add

--- app/src/main/java/home/mva/blog/util/AppExecutors.java
+++ app/src/main/java/home/mva/blog/util/AppExecutors.java
@@ -1,5 +1,9 @@
 package home.mva.blog.util;
 
+import android.os.Handler;
+import android.os.Looper;
+import android.support.annotation.NonNull;
+
 import java.util.concurrent.Executor;
 
 /**
@@ -11,15 +15,32 @@ public class AppExecutors {
 
     private final Executor diskIO;
     //private final Executor networkIO;
-    //private final Executor mainThread;
+    private final Executor mainThread;
 
-    AppExecutors(Executor diskIO/*, Executor networkIO, Executor mainThread*/) {
+    AppExecutors(Executor diskIO/*, Executor networkIO*/, Executor mainThread) {
         this.diskIO = diskIO;
         //this.networkIO = networkIO;
-        //this.mainThread = mainThread;
+        this.mainThread = mainThread;
     }
 
     public AppExecutors() {
-        this(new DiscIOThreadExecutor());
+        this(new DiscIOThreadExecutor(), new MainThreadExecutor());
+    }
+
+    public Executor getDiskIO() {
+        return diskIO;
+    }
+
+    public Executor getMainThread() {
+        return mainThread;
+    }
+
+    private static class MainThreadExecutor implements Executor {
+        private Handler mainThreadHamdler = new Handler(Looper.getMainLooper());
+
+        @Override
+        public void execute(@NonNull Runnable command) {
+            mainThreadHamdler.post(command);
+        }
     }
 }
