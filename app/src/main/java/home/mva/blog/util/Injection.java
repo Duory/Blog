428fcae61969741a0c218d02e6676231e982bc1e
Duory <makovoi94@gmail.com>
2018-01-29 18:53:16 +0700

    Injection class add

---
+++ app/src/main/java/home/mva/blog/util/Injection.java
@@ -0,0 +1,24 @@
+package home.mva.blog.util;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import home.mva.blog.data.source.PostsRepository;
+import home.mva.blog.data.source.local.BlogDatabase;
+import home.mva.blog.data.source.local.PostsLocalDataSource;
+import home.mva.blog.data.source.remote.BlogRemoteDataSource;
+
+/**
+ * Enables injection of production implementations for
+ * PostsDataSource and CommentsDataSource at compile time.
+ */
+
+public class Injection {
+
+    public static PostsRepository providePostsRepository(@NonNull Context context) {
+        BlogDatabase database = BlogDatabase.getInstance(context);
+
+        return PostsRepository.getInstance(BlogRemoteDataSource.getInstance(),
+                PostsLocalDataSource.getInstance(new AppExecutors(), database.getPostDao()));
+    }
+}
