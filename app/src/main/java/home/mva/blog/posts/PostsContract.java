3b2df0b2f5709cac119f26f2793de05a43edf0db
Duory <makovoi94@gmail.com>
2018-01-29 17:21:40 +0700

    posts contract

---
+++ app/src/main/java/home/mva/blog/posts/PostsContract.java
@@ -0,0 +1,43 @@
+package home.mva.blog.posts;
+
+import android.support.annotation.NonNull;
+
+import java.util.List;
+
+import home.mva.blog.base.BasePresenter;
+import home.mva.blog.base.BaseView;
+import home.mva.blog.data.model.Post;
+
+/**
+ * This specifies the contract between the view and the presenter.
+ */
+public interface PostsContract {
+
+    interface View extends BaseView<Presenter> {
+
+        void setLoadingIndicator(boolean active);
+
+        void showPosts(List<Post> posts);
+
+        void showPostDetailsUi(String postId);
+
+        void showAddPost();
+
+        void showSuccessfullySavedMessage();
+
+        void showNoPostsMessage();
+
+        void showLoadingPostsError();
+
+        boolean isActive();
+    }
+
+    interface Presenter extends BasePresenter {
+
+        void loadPosts(boolean forceUpdate);
+
+        void openPost(@NonNull Post requestedPost);
+
+        void addNewPost();
+    }
+}
